ba9a92e2aee40cf8833f05dea650ba5ad70f468a
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-30 10:23:47 +0900

    fix error and layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Top from "./components/Top";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <Router>
       <ul>
         <li>
-          <Link to="/">トップ</Link>
+          <Link to="/">Top</Link>
         </li>
         <li>
-          <Link to="/about/">アバウト</Link>
+          <Link to="/about/">About</Link>
+        </li>
+        <li>
+          <Link to="/contact/">Contact</Link>
         </li>
       </ul>
       <Route path="/" exact component={Top} />
       <Route path="/about/" exact component={About} />
+      <Route path="/contact/" exact component={Contact} />
     </Router>
   );
 }
