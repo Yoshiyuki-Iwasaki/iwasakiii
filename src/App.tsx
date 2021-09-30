592b2b162f09135be1abf4421a45c16102090b8b
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-30 09:29:24 +0900

    add typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Top from './components/Top'
-import About from './components/About'
+import Top from "./components/Top";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <ul>
-        <li><Link to='/'>トップ</Link></li>
-        <li><Link to='/about/'>アバウト</Link></li>
+        <li>
+          <Link to="/">トップ</Link>
+        </li>
+        <li>
+          <Link to="/about/">アバウト</Link>
+        </li>
       </ul>
       <Route path="/" exact component={Top} />
       <Route path="/about/" exact component={About} />
