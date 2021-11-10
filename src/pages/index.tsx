1db7ec9f7316cda386818d2a44c7a3ac7ecbecde
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-10 17:23:58 +0900

    fix layout.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/module/Layout';
 
-export default function Home() {
-  return (
-    <div>Hello world</div>
-  )
+const Home = () => {
+  return <Layout>Home</Layout>;
 }
+
+export default Home;
